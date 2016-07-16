5652579ea6c0b80cce6573522c2eb430a976578b
shubhamagarwal1993 <shubhamagarwal1993@gmail.com>
2016-07-16 16:55:41 -0500

    testing for text replies

--- index.js
+++ index.js
@@ -42,7 +42,10 @@ app.post('/webhook/', function (req, res) {
 				sendGenericMessage(sender)
 				continue
 			}
-			sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+//			sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+			else if (text == 'hi' || 'hello' || 'hey')
+				sendTextMessage(sender, 'Hi, how are you!')
+			sendTextMessage(sender, text.substring(0, 200))
 		}
 		if (event.postback) {
 			let text = JSON.stringify(event.postback)
